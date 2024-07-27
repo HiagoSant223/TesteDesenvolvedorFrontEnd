1c80c2c56f2586abcdb75227dcb116781095dc93
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-27 18:36:32 -0300

    Tailwind Config

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
     <div className='content'>
+      <h1 className='text-red-700'>Consulte seu CNPJ</h1>
       <input type='text'
       id='cnpj-inout'
       placeholder='Degite o CNPJ'></input>
