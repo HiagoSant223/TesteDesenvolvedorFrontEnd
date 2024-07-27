ae590a499cbcd16b00df453d6f7fe1df08198ec5
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-27 18:22:28 -0300

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='content'>
+      <input type='text'
+      id='cnpj-inout'
+      placeholder='Degite o CNPJ'></input>
+    </div>
+    </>
+  )
+}
+
+export default App
