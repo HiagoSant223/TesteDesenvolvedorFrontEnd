d696c124899400571698ec6dc682ce3b1d7c67ec
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-27 19:35:59 -0300

    update function GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const [cnpj, setCnpj] = useState('');
+  const [infoData, setInfoData] = useState(null);
+
+  const handleInputChange = (e) => {
+    setCnpj(e.taget.value);
+  }
+
+  const handleInputSubmit = () => {
+    if (cnpj){
+      fetch('https://brasilapi.com.br/api/cnpj/v1/{{CNPJ}}')
+      .then(response => response.json())
+      .then(data => setInfoData(data))
+      .catch(error => console.error('Erro:', error));
+    }
+  }
+  
   return (
     <>
-    <div className='content'>
-      <h1 className='text-red-700'>Consulte seu CNPJ</h1>
+    <div className='cotainer'>
+      <h1 className='text-6xl'>Consulte o CNPJ</h1>
       <input type='text'
       id='cnpj-inout'
       placeholder='Degite o CNPJ'></input>
