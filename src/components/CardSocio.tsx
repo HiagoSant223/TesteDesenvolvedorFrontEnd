5779f183345352cce7a43ac39ee4d980cf391388
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-28 16:05:59 -0300

    update interface empresa

--- src/components/CardSocio.tsx
+++ src/components/CardSocio.tsx
@@ -1,51 +1,76 @@
-import CardEmpresa from "./CardEmpresa";
 
-function CardSocio({ data }) {
+interface Empresa {
+  nome: string;
+  qualificacao: string;
+}
+
+interface CompanyData {
+  nome_fantasia: string;
+  razao_social: string;
+  data_abertura: string;
+  situacao: string;
+  atividade_principal: { text: string }[];
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  municipio: string;
+  uf: string;
+  cep: string;
+  telefone: string;
+  email: string;
+  qsa: Empresa[];
+}
+
+interface Props {
+  data: CompanyData;
+}
+
+const CardSocio: React.FC<Props> = ({ data }) => {
   return (
     <>
-      <div>
+      <div id="result-container">
+      <div className="result-item">
         <label>Nome Fantasia:</label>
-        <input type="text" defaultValue={data.nome_fantasia || ""} />
+        <input type="text" defaultValue={data.nome_fantasia || ''} />
       </div>
-      <div>
-        <label> Razão Social: </label>
-        <input type="text" defaultValue={data.razao_social || ""} readOnly />
+      <div className="result-item">
+        <label>Razão Social:</label>
+        <input type="text" defaultValue={data.razao_social || ''} readOnly />
       </div>
-      <div>
-        <label>Data de Abertura: </label>
-        <input type="text" defaultValue={data.data_abertura || ""} readOnly />
+      <div className="result-item">
+        <label>Data de Abertura:</label>
+        <input type="text" defaultValue={data.data_abertura || ''} readOnly />
+      </div>
+      <div className="result-item">
+        <label>Situação:</label>
+        <input type="text" defaultValue={data.situacao || ''} readOnly />
       </div>
       <div className="result-item">
         <label>Atividade Principal:</label>
-        <input
-          type="text"
-          defaultValue={data.atividade_principal[0].text || ""}
-          readOnly
-        />
+        <input type="text" defaultValue={data.atividade_principal[0].text || ''} readOnly />
       </div>
       <div className="result-item">
         <label>Endereço Completo:</label>
-        <input
-          type="text"
-          defaultValue={
-            `${data.logradouro}, ${data.numero} ${data.complemento}, ${data.bairro}, ${data.municipio} - ${data.uf}, ${data.cep}` ||
-            ""
-          }
-          readOnly
-        />
+        <input type="text" defaultValue={`${data.logradouro}, ${data.numero} ${data.complemento}, ${data.bairro}, ${data.municipio} - ${data.uf}, ${data.cep}` || ''} readOnly />
       </div>
       <div className="result-item">
         <label>Telefone:</label>
-        <input type="text" defaultValue={data.telefone || ""} readOnly />
+        <input type="text" defaultValue={data.telefone || ''} readOnly />
       </div>
       <div className="result-item">
         <label>Email:</label>
-        <input type="text" defaultValue={data.email || ""} readOnly />
+        <input type="text" defaultValue={data.email || ''} readOnly />
       </div>
-      {data.qsa && data.qsa.map((empresa, index) => (
-          <CardEmpresa key={index} empresa={empresa} />
-        ))}
+      {data.qsa && data.qsa.map((partner, index) => (
+        <div key={index} className="card">
+          <h3>{partner.nome}</h3>
+          <p>Qualificação: {partner.qualificacao}</p>
+        </div>
+      ))}
+    </div>
     </>
   );
 }
+
 export default CardSocio;
