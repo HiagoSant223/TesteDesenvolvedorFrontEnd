dce961c30bdca7f83733c40ea09e90b00978c52e
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-28 23:45:52 -0300

    style componente EmpresaData.tsx

--- src/components/EmpresaData.tsx
+++ src/components/EmpresaData.tsx
@@ -7,45 +7,45 @@ interface Props {
 
 const EmpresaData: React.FC<Props> = ({ data }) => {
   return (
-    <div id="result-container">
-      <div className="result-item">
-        <label>CNPJ:</label>
-        <input type="text" defaultValue={data.cnpj} readOnly />
+    <div className="">
+      <div className='text-center '>
+        <h1>Confira os dados da empresa pesquisada</h1>
       </div>
-      <div className="result-item">
-        <label>Razão Social:</label>
-        <input type="text" defaultValue={data.razao_social} readOnly />
+      <div className="result-item text-center">
+        <label>Nome Fantasia: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.nome_fantasia} />
       </div>
-      <div className="result-item">
-        <label>Nome Fantasia:</label>
-        <input type="text" defaultValue={data.nome_fantasia} />
+      <div className="result-item text-center">
+        <label>Razão Social: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.razao_social} readOnly />
       </div>
-      <div className="result-item">
-        <label>Situação Cadastral:</label>
-        <input type="text" defaultValue={data.descricao_situacao_cadastral} readOnly />
+      <div className="result-item text-center">
+        <label>Data de Abertura: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.data_inicio_atividade} readOnly />
       </div>
-      <div className="result-item">
-        <label>Data de Situação Cadastral:</label>
-        <input type="text" defaultValue={data.data_situacao_cadastral} readOnly />
+      <div className="result-item text-center">
+        <label>Situação Cadastral: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.descricao_situacao_cadastral} readOnly />
       </div>
-      <div className="result-item">
-        <label>Data de Início de Atividade:</label>
-        <input type="text" defaultValue={data.data_inicio_atividade} readOnly />
+      <div className="result-item text-center">
+        <label>Atividade Principal: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.cnae_fiscal_descricao} readOnly />
       </div>
-      <div className="result-item">
-        <label>Atividade Principal:</label>
-        <input type="text" defaultValue={data.cnae_fiscal_descricao} readOnly />
+      <div className="result-item text-center">
+        <label>Endereço Completo: </label>
+        <input className='bg-transparent' type="text" defaultValue={`${data.logradouro}, ${data.numero} ${data.complemento}, ${data.bairro}, ${data.municipio} - ${data.uf}, ${data.cep}`} readOnly />
       </div>
-      <div className="result-item">
-        <label>Endereço Completo:</label>
-        <input type="text" defaultValue={`${data.logradouro}, ${data.numero} ${data.complemento}, ${data.bairro}, ${data.municipio} - ${data.uf}, ${data.cep}`} readOnly />
+      <div className="result-item text-center">
+        <label>Telefone: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.ddd_telefone_1} readOnly />
       </div>
-      <div className="result-item">
-        <label>Telefone:</label>
-        <input type="text" defaultValue={data.ddd_telefone_1} readOnly />
+      <div className="result-item text-center">
+        <label>CNPJ: </label>
+        <input className='bg-transparent' type="text" defaultValue={data.cnpj} readOnly />
       </div>
     </div>
   );
+  
 }
 
 export default EmpresaData;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -22,38 +22,44 @@ function Input() {
   };
 
   return (
-    <div className="container mx-auto flex flex-col items-center mt-8">
-      <div className="titulo mb-4">
+    <div className="w-full h-screen bg-[#20303C] flex flex-col">
+      <div className="fixed top-0 left-0 w-full bg-[#20303C] p-4 flex flex-col items-center z-10">
         <h1 className="text-6xl text-center text-white font-extralight">
           CNPJ CONSULTA
         </h1>
-      </div>
-      <div className="flex items-center space-x-4 w-3/5">
-        <div className="relative w-full flex items-center">
-          <div className="absolute text-white left-3">
-            <FaSearch />
+        <div className="flex items-center space-x-4 w-full max-w-3xl mt-4">
+          <div className="relative w-full flex items-center">
+            <div className="absolute text-white left-3">
+              <FaSearch />
+            </div>
+            <input
+              type="text"
+              value={cnpj}
+              onChange={handleInputChange}
+              placeholder="Digite o CNPJ sem pontuação"
+              className="text-white bg-transparent border pl-10 p-2 rounded-lg w-full"
+            />
           </div>
-          <input
-            type="text"
-            value={cnpj}
-            onChange={handleInputChange}
-            placeholder="Digite o CNPJ"
-            className="text-white bg-transparent border pl-10 p-2 rounded-lg w-full my-10 "
-          />
+          <button
+            onClick={handleInputSubmit}
+            className="p-2 bg-white text-black rounded"
+          >
+            Consultar
+          </button>
         </div>
-        <button
-          onClick={handleInputSubmit}
-          className="p-2 bg-white text-black rounded"
-        >
-          Consultar
-        </button>
       </div>
-      {infoData && (
-        <div className="mt-8">
-          <EmpresaData data={infoData} />
-          <CardSocio partners={infoData.qsa} />
-        </div>
-      )}
+      <div className="pt-32 flex-1 overflow-y-auto">
+        {infoData && (
+          <div className="mt-8">
+            <div className="">
+              <EmpresaData data={infoData} />
+            </div>
+            <div className="p-4 ">
+              <CardSocio partners={infoData.qsa} />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
