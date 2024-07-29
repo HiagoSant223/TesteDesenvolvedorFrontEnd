137dd4ece28ab385c8b82945f481566cd25d8a4a
Hiago Oliveira <hiagosantos223@gmail.com>
2024-07-28 21:20:32 -0300

    style Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import EmpresaData from "./EmpresaData";
 import CardSocio from "./CardSocio";
 import CompanyData from "../Interface/CompanyData";
+import { FaSearch } from "react-icons/fa";
 
 function Input() {
   const [cnpj, setCnpj] = useState<string>("");
@@ -21,22 +22,37 @@ function Input() {
   };
 
   return (
-    <div className="container">
-      <h1 className="text-6xl">Consulta de CNPJ</h1>
-      <div>
-        <input
-          type="text"
-          value={cnpj}
-          onChange={handleInputChange}
-          placeholder="Digite o CNPJ"
-        />
-        <button onClick={handleInputSubmit}>Consultar</button>
+    <div className="container mx-auto flex flex-col items-center mt-8">
+      <div className="titulo mb-4">
+        <h1 className="text-6xl text-center text-white">CNPJ CONSULTA</h1>
+      </div>
+      <div className="flex items-center space-x-4 w-3/5">
+        <div className="relative w-full">
+          <div className="absolute rounded-full inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <div className="text-white">
+              <FaSearch />
+            </div>
+          </div>
+          <input
+            type="text"
+            value={cnpj}
+            onChange={handleInputChange}
+            placeholder="Digite o CNPJ"
+            className="text-white pl-10 p-2 border-0 rounded-lg bg-transparent shadow-input-focus ring-offset-input-focus w-full"
+          />
+        </div>
+        <button
+          onClick={handleInputSubmit}
+          className="p-2 bg-white text-black rounded"
+        >
+          Consultar
+        </button>
       </div>
       {infoData && (
-        <>
+        <div className="mt-8">
           <EmpresaData data={infoData} />
           <CardSocio partners={infoData.qsa} />
-        </>
+        </div>
       )}
     </div>
   );
